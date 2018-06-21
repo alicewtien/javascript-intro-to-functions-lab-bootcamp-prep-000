3463b215a92bd5e0e70928f41cb1abf89fda7e88
Ali T <alicewtien@gmail.com>
2018-06-21 20:38:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,16 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
+  if (string.toLowerCase())
   return "I can't hear you!"
-  if (string.toLowerCase()
+}
+
+function sayHiToGrandma(string) {
+  if (string.toUpperCase())
+  return "YES INDEED!"
+}
+
+function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma.")
+  return "I love you, too."
 }
